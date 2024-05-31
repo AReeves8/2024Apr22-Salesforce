bf0236377cfa428d803893e02b5e472181ca076d
AReeves8 <areeves@skillstorm.com>
2024-05-31 14:20:48 -0500

    lesson 14

--- DEX450_Exercises/force-app/main/default/lwc/certChecker/certChecker.js
+++ DEX450_Exercises/force-app/main/default/lwc/certChecker/certChecker.js
@@ -1,3 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
 
-export default class CertChecker extends LightningElement {}
+export default class CertChecker extends LightningElement {
+
+    @api
+    recordId;
+    cardTitle = 'Awaiting Record ID';
+    techIds;
+
+    @wire(getRecord, { recordId : '$recordId', fields : [ACCOUNT_NAME_FIELD]})
+    wiredAccount(result) {
+        if(result.data) {
+            this.cardTitle = getFieldValue(result.data, ACCOUNT_NAME_FIELD) + ' Technicians';
+        }
+        else if(result.error) {
+            this.cardTitle = 'Could not retreive account name with record id = ' + this.recordId;
+        }   
+    }
+
+
+    handleSelectedTechIds(event){
+        this.techIds = event.detail.techIds;
+    }
+}
--- DEX450_Exercises/force-app/main/default/lwc/techsCertsHeld/techsCertsHeld.js
+++ DEX450_Exercises/force-app/main/default/lwc/techsCertsHeld/techsCertsHeld.js
@@ -1,3 +1,82 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getCertsHeld from '@salesforce/apex/CertCheckerController.getCertsHeld';
+import deleteCertsHeld from '@salesforce/apex/CertCheckerController.deleteCertsHeld';
+import { refreshApex } from '@salesforce/apex';
 
-export default class TechsCertsHeld extends LightningElement {}
+
+export default class TechsCertsHeld extends LightningElement {
+
+    @api
+    accRecordId;
+    errorMessage;
+    rowData = [];
+    colData = [
+        {label: 'Technician', fieldName: 'TechName', type: 'text'},
+        {label: 'Certification', fieldName: 'CertName', type: 'text'},
+        {label: 'Date Achieved', fieldName: 'DateAchieved', type: 'date'}
+    ];
+    selectedRows = [];
+    disableDeleteBtn = true;
+    wiredResult;
+
+    @wire(getCertsHeld, { acctId : '$accRecordId' })
+    wiredCertsHeld(result) {
+
+        this.wiredResult = result; 
+
+        if(result.data) {
+            this.rowData = result.data.map((record) => ({
+                Id : record.Id,
+                TechName : record.Certified_Professional__r.Name,
+                TechId: record.Certified_Professional__c,
+                CertName: record.Certification__r.Name,
+                CertId : record.Certification__c,
+                DateAchieved : record.Date_Achieved__c
+            }));
+
+            //if we have a success, we want to ensure no errors are displayed
+            this.errorMessage = null;
+        }
+        else if(result.error) {
+            this.errorMessage = result.error.body.message;
+        }
+    }
+
+
+    handleRowSelection(event) {
+
+        let ids;
+
+        if(event.detail.selectedRows.length > 0){
+            this.selectedRows = event.detail.selectedRows;
+            ids = this.selectedRows.map((row) => row.TechId);
+
+            this.disableDeleteBtn = false;
+        }
+        else {
+            this.disableDeleteBtn = true;
+        }
+
+        const evt = new CustomEvent('selectedtechids', {
+            detail : {
+                techIds : ids
+            }
+        });
+        this.dispatchEvent(evt);
+    }
+
+    handleDeleteButton(event) {
+
+        console.log('delete clicked');
+        let ids = this.selectedRows.map((row) => row.Id);
+
+        deleteCertsHeld({ certIds : ids })
+            .then((result) => {
+                refreshApex(this.wiredResult);
+            })
+            .catch((error)=> {
+                this.errorMessage = error.body.message;
+            });
+
+    }
+}
