6ad2611e1c77c1e064a0e8388aa2dbf97b5325e1
AReeves8 <areeves@skillstorm.com>
2024-06-04 13:21:10 -0500

    finished demo

--- lwc-component-communication/force-app/main/default/lwc/objectSelector/objectSelector.js
+++ lwc-component-communication/force-app/main/default/lwc/objectSelector/objectSelector.js
@@ -1,4 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import OBJECT_SEARCH_CHANNEL from '@salesforce/messageChannel/objectSearchChannel__c';
+import { MessageContext, publish } from 'lightning/messageService';
 
 export default class ObjectSelector extends LightningElement {
 
@@ -10,6 +12,10 @@ export default class ObjectSelector extends LightningElement {
         {label : 'Lead', value : 'Lead'}
     ];
 
+    @wire(MessageContext)
+    messageContext;
+
+
     // handles the onChange event from the combobox
     handleSelection(event) {
         this.objectSelected = true;
@@ -18,7 +24,15 @@ export default class ObjectSelector extends LightningElement {
     }
 
     handleFields(event) {
-        console.log('FIELDS BEING HANDLED');
+        const payload = {
+            object : this.selectedObject,
+            fields : event.detail.selectedFields
+        }
+
+        console.log('PAYLOAD: ' + payload);
+
+        // sending object and field data across the lightning message service
+        publish(this.messageContext, OBJECT_SEARCH_CHANNEL, payload);
     }
 
 }
--- lwc-component-communication/force-app/main/default/lwc/searchResults/searchResults.js
+++ lwc-component-communication/force-app/main/default/lwc/searchResults/searchResults.js
@@ -1,3 +1,66 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import OBJECT_SEARCH_CHANNEL from '@salesforce/messageChannel/objectSearchChannel__c';
+import { MessageContext, subscribe } from 'lightning/messageService';
+import getRecords from '@salesforce/apex/DynamicObjectSearchController.getRecords'
 
-export default class SearchResults extends LightningElement {}
+export default class SearchResults extends LightningElement {
+
+
+    selectedObject;
+    selectedFields;
+    errorMessage;
+    toggleTable = false;
+
+    tableRowData = [];
+    tableColData = [];
+
+    subscription = null;
+
+    @wire(MessageContext)
+    messageContext;             // property containing context to lightning message service
+
+    // calling an apex method to retrieve records from the org
+    @wire(getRecords, {objectName : '$selectedObject', fieldNames : '$selectedFields'}) 
+    wiredRecords(result) {
+        if(result.data) {
+
+            // setting the table data
+            this.tableRowData = result.data;
+
+            // ensuring the error message does not display
+            this.errorMessage = null;
+            this.toggleTable = true;
+        }
+        else if(result.error) {
+
+            // setting the error message
+            this.errorMessage = 'Data could not be retrieved.';
+            this.toggleTable = false;
+        }
+    }
+
+    // component lifecycle method that runs after the componet is rendered
+    connectedCallback() {
+
+        // setting this component to subscribe to the message channel as soon as it is loaded
+        this.subscription = subscribe(
+            this.messageContext,
+            OBJECT_SEARCH_CHANNEL,
+            (message) => this.handleMessage(message)
+        );
+    }
+
+    handleMessage(message) {
+        this.selectedObject = message.object;
+        this.selectedFields = message.fields;
+
+        console.log('OBJECT:' + this.selectedObject + '\nFIELDS: ' + this.selectedFields.toString());
+
+        // setting columns with field data and formatting data to be used by lightning-datatable
+        this.tableColData = this.selectedFields.map((element) => ({
+            label : element,
+            fieldName: element,
+            type : 'text'
+        }));
+    }
+}
